3fdc2c94c67997e60e97141fae991da7d9ca0649
Malte Bublitz <kontakt@malte-bublitz.de>
2014-01-13 14:07:33 +0100

    Added a simple test command to the module

--- show_my_ip/__init__.py
+++ show_my_ip/__init__.py
@@ -6,7 +6,7 @@ from get_local import get_ip as get_local_ip
 
 if __name__=='__main__':
 	import sys
-	if len(sys.argv)==2 and not sys.argv[1] in ("--help", "--version", "-h", "-V"):
+	if len(sys.argv)>=2 and not sys.argv[1] in ("--help", "--version", "-h", "-V"):
 		if sys.argv[1]=="global":
 			print("Global IP:")
 			_global = get_global_ip()
@@ -16,6 +16,11 @@ if __name__=='__main__':
 			_local = get_local_ip()
 			for ip in _local:
 				print(str(ip))
+		elif sys.argv[1]=="test":
+			if len(sys.argv)==3:
+				print(str(IP(sys.argv[2])))
+			else:
+				print(str(IP("178.63.11.250")))
 		else:
 			print "Unknown command."
 	else:
